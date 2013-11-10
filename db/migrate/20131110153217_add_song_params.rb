11e7948e868515f1473f927235b0eb37d051f5d6
Danny Kirschner <rkirschner1377@gmail.com>
2013-11-10 16:15:46 -0500

    delete problem migration

--- db/migrate/20131110153217_add_song_params.rb
+++ db/migrate/20131110153217_add_song_params.rb
@@ -1,20 +1,5 @@
 class AddSongParams < ActiveRecord::Migration
   def change
-    change_table :songs do |t|
-      t.string :item
-      t.text :description
-      t.boolean :is_preview
-      t.string :site_name
-      t.string :kind
-      t.string :url
-      t.string :video_url
-      t.string :audio_url
-      t.string :height
-      t.string :width
-      t.string :image
-      t.string :mime_type
 
-    end
-    add_index :songs, :user_id
   end
 end
